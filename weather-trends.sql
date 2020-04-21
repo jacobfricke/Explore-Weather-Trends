451bdb7142e7ce224df818e0cde12e3293f933a4
Jacob Fricke <jacob.fricke@gmail.com>
2020-04-21 23:11:59 +0200

    feat: export global and city data

--- weather-trends.sql
+++ weather-trends.sql
@@ -3,3 +3,18 @@
 SELECT *
   FROM city_list
  WHERE country = 'Germany'
+
+# Export city data for Hamburg
+
+SELECT year,
+       avg_temp
+  FROM city_data
+ WHERE city = 'Hamburg'
+ORDER BY year ASC;
+
+# Export global data
+
+SELECT year, 
+       avg_temp
+  FROM global_data
+ORDER BY year ASC;
